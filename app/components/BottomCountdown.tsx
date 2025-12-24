2ad90c39053c604230ae825159e326435a168a87
Brad Potterbaum <bpotterbaum@gmail.com>
2025-12-23 18:39:05 -0600

    Fix optional prop type for onReachZero

--- app/components/BottomCountdown.tsx
+++ app/components/BottomCountdown.tsx
@@ -5,7 +5,7 @@ import { useEffect, useMemo, useState, useRef } from "react";
 type BottomCountdownProps = {
   label?: string;
   targetTimeMs: number; // epoch millis
-  onReachZero: boolean?;
+  onReachZero?: boolean;
 };
 
 function clampSeconds(ms: number) {
